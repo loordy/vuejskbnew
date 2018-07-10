ff93e5f1f5109086c2e35769d64086806b9a22b0
James <takikvara@gmail.com>
2018-07-10 12:06:17 +0300

    merge

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -29,11 +29,6 @@ export default {
   methods: {
     CurrentUserShow () {
       var self = this
-      console.log(this)
-      console.log(this)
-      console.log(this)
-      console.log(this)
-      console.log(this)
       BX24.callMethod('user.current', {}, function (res) {
         console.log(res.data())
         self.msg = res.data().NAME + ' ' + res.data().LAST_NAME
