fee685f959461a87e375548947acc1384e2fbb59
James <takikvara@gmail.com>
2018-07-20 15:38:29 +0300

    Merge branch 'danila' of https://github.com/loordy/vuejskbnew into fBeck
    # Conflicts:
    #	src/components/css/knowledgebase.css

--- src/components/pages/section.vue
+++ src/components/pages/section.vue
@@ -1,5 +1,6 @@
 <template>
   <table style="width:100%;">
+    <empty v-show="(elements.length + sections.length) === 0"></empty>
     <filelist v-show="this.$store.state.settings.viewType === 'list'"></filelist>
     <fileView v-show="this.$store.state.settings.viewType === 'grid'"></fileView>
   </table>
@@ -11,6 +12,14 @@ import fileView from '../FileView'
 import empty from '../empty'
 export default {
   name: 'mainpage',
-  components: {fileView, Filelist, empty}
+  components: {fileView, Filelist, empty},
+  computed: {
+    elements () {
+      return this.$store.getters.getElementsByParentID(this.$route.params.id)
+    },
+    sections () {
+      return this.$store.getters.getSectionsByParentID(this.$route.params.id)
+    }
+  }
 }
 </script>
